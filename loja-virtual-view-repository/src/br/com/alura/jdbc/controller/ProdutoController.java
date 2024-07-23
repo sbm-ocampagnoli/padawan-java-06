3d27a34cded2ab3ef115b65ebb715f1387f2723a
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-23 10:47:20 -0300

    Revisando o projeto

--- loja-virtual-view-repository/src/br/com/alura/jdbc/controller/ProdutoController.java
+++ loja-virtual-view-repository/src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -1,11 +1,21 @@
 package br.com.alura.jdbc.controller;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class ProdutoController {
+
+	private ProdutoDAO produtoDAO;
+
+	public ProdutoController() {
+		Connection connection = new ConnectionFactory().recuperarConexao();
+		this.produtoDAO = new ProdutoDAO(connection);
+	}
 
 	public void deletar(Integer id) {
 		System.out.println("Deletando produto");
@@ -16,10 +26,8 @@ public class ProdutoController {
 	}
 
 	public List<Produto> listar() {
-		List<Produto> produtos = 
-				new ArrayList<Produto>();
-		produtos.add(new Produto("Nome do Produto de teste"
-				, "Descrição do produto de teste"));
+		List<Produto> produtos = new ArrayList<Produto>();
+		produtos.add(new Produto("Nome do Produto de teste", "Descriï¿½ï¿½o do produto de teste"));
 		return produtos;
 	}
 
